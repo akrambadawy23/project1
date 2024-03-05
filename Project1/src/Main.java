b332cd81dbd56201a9f84b3846c0a01dc80f55d5
Ayman Badawy <abadawy598@gmail.com>
2024-03-05 01:30:26 -0500

    Update Main.java

--- Project1/src/Main.java
+++ Project1/src/Main.java
@@ -8,84 +8,71 @@ import java.util.Random;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
 
-
-static File f1file = new File("f1.txt");
-static  File f2file = new File("f2.txt");
-static  File f3file = new File("f3.txt");
-static  File f4file = new File("f4.txt");
-static  File f5file = new File("f5.txt");
-static File f6file = new File("f6.txt");
+    static File f1file = new File("f1.txt");
+    static  File f2file = new File("f2.txt");
+    static  File f3file = new File("f3.txt");
+    static  File f4file = new File("f4.txt");
+    static  File f5file = new File("f5.txt");
+    static File f6file = new File("f6.txt");
 
 
 
     public static void main(String[] args) {
 
-File[] files = {f1file, f2file, f3file, f4file, f5file, f6file};
-
-
+        File[] files = {f1file, f2file, f3file, f4file, f5file, f6file};
 
         //System.out.println(readFile(f1file));
 
-   /* for (int i = 0; i < files.length; i++) {
-        long startTime = System.nanoTime();
-        ArrayList<Integer> list = readFile(files[i]);
-        System.out.println("INVERSIONS: " + QuickSort(list));
-        // if(i < 2)
-        //System.out.println(list);
+        //MERGESORT OUTPUT
 
-        long endTime = System.nanoTime();
-        System.out.println("f" + (i + 1) + " TIME RUN: " + (endTime - startTime) / 1000000 + " MILLISECONDS");
-
-    }
-*/
-        for (int i = 0; i < files.length; i++) {
+        /*for (int i = 0; i < files.length; i++) {
             long startTime = System.nanoTime();
             ArrayList<Integer> list = readFile(files[i]);
             System.out.println("Inversions " + mergeSort(list));
-            // if(i < 2)
-            //System.out.println(list);
-
-            long endTime = System.nanoTime();
-            System.out.println("f" + (i + 1) + " TIME RUN: " + (endTime - startTime) / 1000000 + " MILLISECONDS");
-
-        }
-        for (int i = 0; i < files.length; i++) {
-            long startTime = System.nanoTime();
-            ArrayList<Integer> list = readFile(files[i]);
-
-                System.out.println("INVERSIONS: " + BubbleSort(list));
-                // if(i < 2)
-                System.out.println(list);
-
-
             long endTime = System.nanoTime();
             System.out.println("f" + (i + 1) + " TIME RUN: " + (endTime - startTime) / 1000000000 + " SECONDS");
 
         }
+*/
+        /*
+
+        //BUBBLE SORT OUTPUT
+
+        for (int i = 0; i < files.length; i++) {
+            long startTime = System.nanoTime();
+            ArrayList<Integer> list = readFile(files[i]);
+            System.out.println("INVERSIONS: " + BubbleSort(list));
+            long endTime = System.nanoTime();
+            System.out.println("f" + (i + 1) + " TIME RUN: " + (endTime - startTime) / 1000000000 + " SECONDS");
+        }
+
+ */
+
+        //QUICK SORT OUTPUT
 
 
-
-
-
-
-
-
+    for (int i = 0; i < files.length; i++) {
+        long startTime = System.nanoTime();
+        ArrayList<Integer> list = readFile(files[i]);
+        System.out.println("INVERSIONS: " + HeapSort.heapSort(list, 0));
+        long endTime = System.nanoTime();
+        System.out.println("f" + (i + 1) + " TIME RUN: " + (endTime - startTime) / 1000000000 + " SECONDS");
+    }
 
 
 
     }
-
     public static ArrayList<Integer> readFile(File f) {
         ArrayList<Integer> fin = new ArrayList<Integer>();
         try(BufferedReader br = new BufferedReader(new FileReader(f))) {
 //StringBuilder s = new StringBuilder();
-String num = br.readLine();
-while(num != null) {
-    fin.add(Integer.parseInt(num));
-    //s.append(num);
-    //s.append(System.lineSeparator());
-    num = br.readLine();
-}
+            String num = br.readLine();
+            while(num != null) {
+                fin.add(Integer.parseInt(num));
+                //s.append(num);
+                //s.append(System.lineSeparator());
+                num = br.readLine();
+            }
         } catch (IOException e) {
             System.out.println(f.toString() + " not found");
             System.exit(-1);
@@ -97,14 +84,14 @@ while(num != null) {
 
     public static long BubbleSort(ArrayList<Integer> list) {
         long count = 0;
-boolean swapped;
-        for(int i = 0; i < list.size()-1; i++) {
+        boolean swapped;
+        for(int i = 0; i < list.size(); i++) {
             swapped = false;
-            for(int j = 0; j < list.size()-i-1; j++) {
-                if(list.get(j+1).compareTo(list.get(j)) < 0) {
+            for(int j = i+1; j < list.size(); j++) {
+                if(list.get(i).compareTo(list.get(j)) > 0) {
                     int temp = list.get(j);
-                    list.set(j, list.get(j+1));
-                    list.set(j+1, temp);
+                    list.set(j, list.get(i));
+                    list.set(i, temp);
                     count++;
                     swapped = true;
                 }
@@ -112,77 +99,13 @@ boolean swapped;
             if(!swapped)
                 break;
         }
-
-
-
         return count;
     }
 
 
 
-    public static int QuickSort(ArrayList<Integer> file) {
-        //Collections.shuffle(file);
-        return sort(file, 0, file.size()-1, 0);
-    }
-
-    public static int sort(ArrayList<Integer> a, int lo, int hi, int count) {
-        //int count = 0;
-        if(hi <= lo) return count;
-        int lt = lo;
-        int gt = hi;
-        //int mid = (int)(lo + (double)((hi-lo)/2));
-        //if(a.get(hi) < a.get(lo))
-            //Collections.swap(a, lo, hi);
-Random rand = new Random();
-int b;
-b = rand.nextInt(lo, hi);
-//Collections.swap(a, b, lo);
-
-        int v = a.get(lo);
-        if(a.get(hi) < a.get(lo))
-            v = a.get(hi);
-        //System.out.println(v);
-
-        for(int j = lo; j < hi; j++) {
-            if(a.get(j).compareTo(a.get(j+1)) > 0)
-                count++;
-        }
-
-        int i = lo+1;
-        while(i <= gt) {
 
 
-
-            if(a.get(i).compareTo(v) < 0) {
-                int temp = a.get(lt);
-                a.set(lt, a.get(i));
-                a.set(i, temp);
-//count++;
-                lt++;
-                i++;
-            } else if(a.get(i).compareTo(v) > 0) {
-                int temp = a.get(i);
-                a.set(i, a.get(gt));
-                a.set(gt, temp);
-                //count++;
-                gt--;
-            }
-            else
-                i++;
-
-
-        }
-        sort(a, lo, lt-1, count);
-        sort(a, gt+1, hi, count);
-        return count;
-    }
-
-
-    public static int HeapSort(ArrayList<Integer> file) {
-        int count = 0;
-
-        return count;
-    }
 
     private static long merge(ArrayList<Integer> a, ArrayList<Integer> aux, int lo, int mid, int hi) {
         // precondition: a[lo .. mid] and a[mid+1 .. hi] are sorted subarrays
@@ -205,7 +128,7 @@ b = rand.nextInt(lo, hi);
                 //a[k] = aux[j++];
             else if (j > hi)
                 a.set(k, aux.get(i++));
-            //a[k] = aux[i++];
+                //a[k] = aux[i++];
             else if (aux.get(j) < aux.get(i)) {
                 a.set(k, aux.get(j++));
                 count += mid-i+1;
@@ -215,7 +138,7 @@ b = rand.nextInt(lo, hi);
                 a.set(k, aux.get(i++));
             }
         }
-return count;
+        return count;
         // postcondition: a[lo .. hi] is sorted
     }
 
@@ -224,9 +147,9 @@ return count;
         long count = 0;
         if (hi <= lo) return count;
         int mid = lo + (hi - lo) / 2;
-     count+= mergeSort(a, aux, lo, mid);
-       count+= mergeSort(a, aux, mid + 1, hi);
-     count+=  merge(a, aux, lo, mid, hi);
+        count+= mergeSort(a, aux, lo, mid);
+        count+= mergeSort(a, aux, mid + 1, hi);
+        count+=  merge(a, aux, lo, mid, hi);
 
         return  count;
     }
@@ -236,8 +159,110 @@ return count;
      * @param a the array to be sorted
      */
     public static long mergeSort(ArrayList<Integer> a) {
-
         ArrayList<Integer> aux = new ArrayList<Integer>(a);
         return mergeSort(a, aux, 0, a.size()-1);
     }
+
+    /* Quick Sort
+
+    public static long sort(ArrayList<Integer> a, int lo, int hi, long count) {
+
+        if(hi <= lo) return count;
+
+        int lt = lo;
+        int gt = hi;
+
+        Random rand = new Random();
+
+        int v = a.get(lo);
+        if(a.get(hi) < a.get(lo))
+            v = a.get(hi);
+
+        int i = lo+1;
+
+        while(i <= gt) {
+            if(a.get(i).compareTo(v) < 0) {
+
+                int temp = a.get(lt);
+                a.set(lt, a.get(i));
+                a.set(i, temp);
+                count+=(i-lt)+1;
+                lt++;
+                i++;
+            } else if(a.get(i).compareTo(v) > 0) {
+                int temp = a.get(i);
+                a.set(i, a.get(gt));
+                a.set(gt, temp);
+                count+=(gt-i);
+                gt--;
+            }
+            else
+                i++;
+        }
+        sort(a, lo, lt-1, count);
+        sort(a, gt+1, hi, count);
+        return count;
+    }
+
+    public static long QuickSort(ArrayList<Integer> file) {
+        //Collections.shuffle(file);
+        return sort(file, 0, file.size()-1, 0);
+    }
+    */
+    //HeapSort
+
+    public static class HeapSort{
+
+        static long inversionsCount = 0;
+        public static long heapSort(ArrayList<Integer> pq, long inversionsCount) {
+
+            int n = pq.size();
+
+            // heapify phase
+            for (int k = n/2; k >= 1; k--)
+                sink(pq, k, n);
+
+            // sortdown phase
+            int k = n;
+            while (k > 1) {
+                inversionsCount = exch(pq, 1, k--);
+                sink(pq, 1, k);
+            }
+            return inversionsCount;
+        }
+
+        /***************************************************************************
+         * Helper functions to restore the heap invariant.
+         ***************************************************************************/
+
+        private static void sink(ArrayList<Integer> pq, int k, int n) {
+            while (2*k <= n) {
+                int j = 2*k;
+                if (j < n && less(pq, j, j+1)) j++;
+                if (!less(pq, k, j)) break;
+                exch(pq, k, j);
+                k = j;
+            }
+        }
+
+        /***************************************************************************
+         * Helper functions for comparisons and swaps.
+         * Indices are "off-by-one" to support 1-based indexing.
+         ***************************************************************************/
+        private static boolean less(ArrayList<Integer> pq, int i, int j) {
+            return pq.get(i-1).compareTo(pq.get(j-1)) < 0;
+        }
+
+        private static long exch(ArrayList<Integer> pq, int i, int j) {
+            int swap = pq.get(i-1);
+            pq.set(i-1, pq.get(j-1));
+            pq.set(j-1, swap);
+            return inversionsCount+=(j - i);
+        }
+    }
+
+
+
+    // Driver program
 }
+
